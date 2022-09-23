289a5a42f08e775d73f44316ef328108246e19c6
JuliusGuimtee <113960756+JuliusGuimtee@users.noreply.github.com>
2022-09-23 23:37:36 +0800

    Add files via upload
    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
